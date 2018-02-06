610f837f842ca8aac46b3a0b37e873618eae0c2c
Luiz Fonseca <eu@luiz.cc>
2018-02-06 19:47:30 -0200

    Add initial commit with mix setup files

---
+++ lib/recrop.ex
@@ -0,0 +1,18 @@
+defmodule Recrop do
+  @moduledoc """
+  Documentation for Recrop.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Recrop.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/recrop_test.exs
@@ -0,0 +1,8 @@
+defmodule RecropTest do
+  use ExUnit.Case
+  doctest Recrop
+
+  test "greets the world" do
+    assert Recrop.hello() == :world
+  end
+end
